0ce5449c178d651faad8c071a13728492c4af7d7
xtemparmonkx <gastondavids@gmail.com>
2022-08-29 02:58:59 -0300

    1h

--- DesafioClase14/Assets/Scripts/Managers/PowerUpManager.cs
+++ DesafioClase14/Assets/Scripts/Managers/PowerUpManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class PowerUpManager : MonoBehaviour
 {
@@ -14,15 +17,39 @@ public class PowerUpManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerUps[0].SetActive(true);
+        powerUps[0].transform.parent = tankTop;
+        powerUps[0].transform.position = Vector3.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    void DiseableAllPowerUps()
     {
-        
+
+        for (int i = 0; i < powerUps.Length; i++)
+        {
+            powerUps[i].SetActive(false); // 0 -> Weapon A  1->WB / 2 -> WC
+        }
     }
 
-    
-    
+    private void HavePowerUp(GameObject powerUp)
+    {
+        UnequipPowerUps();
+        powerUp.SetActive(true);
+        powerUp.transform.parent = tankTop;
+        powerUp.transform.localPosition = Vector3.zero;
+    }
+
+    private void UnequipPowerUps()
+    {
+        {
+            //foreach para recorrer todos los hijos.
+            foreach (Transform child in tankTop)
+            {
+                child.parent = null;
+                //child.transform.position = new Vector3(Random.Range(0f, 3f), 1f, Random.Range(0f, 3f));
+                child.transform.position = new Vector3(Random.Range(0f, 3f), 1f, Random.Range(0f, 3f));
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
 }
