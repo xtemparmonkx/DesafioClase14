e0647854d7688968839970f6572d990288fec1b3
xtemparmonkx <gastondavids@gmail.com>
2022-08-29 20:09:59 -0300

    1

--- DesafioClase14/Assets/Scripts/Managers/PowerUpManager.cs
+++ DesafioClase14/Assets/Scripts/Managers/PowerUpManager.cs
@@ -11,15 +11,46 @@ public class PowerUpManager : MonoBehaviour
 
     [SerializeField] Transform tankTop;
 
-    [SerializeField] List<GameObject> powerUpsList;
+    [SerializeField] List<GameObject> powerUpList;
+    public List<GameObject> PowerUpList { get => powerUpList; set => powerUpList = value; }
 
-    public List<GameObject> PowerUpsList { get => powerUpsList; set => powerUpsList = value; }
+    private Queue powerUpQueue;
+    public Queue PowerUpQueue { get => powerUpQueue; set => powerUpQueue = value; }
+    
+    private Stack powerUpStack;
+    public Stack PowerUpStack { get => powerUpStack; set => powerUpStack = value; }
+    public Dictionary<string, GameObject> PowerUpDirectory { get => powerUpDirectory; set => powerUpDirectory = value; }
+
+    private Dictionary<string, GameObject> powerUpDirectory;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        powerUps[0].SetActive(true);
-        powerUps[0].transform.parent = tankTop;
-        powerUps[0].transform.position = Vector3.zero;
+        //powerUps[0].SetActive(true);
+        //powerUps[0].transform.parent = tankTop;
+        //powerUps[0].transform.position = Vector3.zero;
+        powerUpQueue = new Queue();
+        powerUpList = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (IsQueueEmpty())
+            {
+                GameObject powerUp = powerUpQueue.Dequeue() as GameObject;
+                EquipPowerUp(powerUp);
+            }
+        }
+    }
+
+    private bool IsQueueEmpty()
+    {
+        return powerUpQueue.Count > 0;
     }
 
     void DiseableAllPowerUps()
@@ -31,14 +62,14 @@ public class PowerUpManager : MonoBehaviour
         }
     }
 
-    private void HavePowerUp(GameObject powerUp)
+    private void EquipPowerUp(GameObject powerUp)
     {
-        UnequipPowerUps();
+        //UnequipPowerUps();
         powerUp.SetActive(true);
         powerUp.transform.parent = tankTop;
         powerUp.transform.localPosition = Vector3.zero;
-    }
-
+    }     
+    
     private void UnequipPowerUps()
     {
         {
@@ -46,7 +77,6 @@ public class PowerUpManager : MonoBehaviour
             foreach (Transform child in tankTop)
             {
                 child.parent = null;
-                //child.transform.position = new Vector3(Random.Range(0f, 3f), 1f, Random.Range(0f, 3f));
                 child.transform.position = new Vector3(Random.Range(0f, 3f), 1f, Random.Range(0f, 3f));
                 child.gameObject.SetActive(true);
             }
---
+++ DesafioClase14/Assets/Scripts/Player/Collisions.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+    [SerializeField] PowerUpManager powerUpManager;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            other.gameObject.SetActive(false);
+            powerUpManager.PowerUpList.Add(other.gameObject);
+            //COLA
+            powerUpManager.PowerUpQueue.Enqueue(other.gameObject);
+            Debug.Log("ELEMENTOS EN LA COLA " + powerUpManager.PowerUpQueue.Count);
+            //STACK
+            //powerUpManager.PowerUpStack.Push(other.gameObject);
+            //Debug.Log("ELEMENTOS EN LA STACK " + powerUpManager.PowerUpStack.Count);
+            //DIC
+           // if (!powerUpManager.PowerUpDirectory.ContainsKey(other.gameObject.name))
+            //{
+            //    powerUpManager.PowerUpDirectory.Add(other.gameObject.name, other.gameObject);
+            //    Debug.Log(powerUpManager.PowerUpDirectory[other.gameObject.name]);
+            //}
+            
+        }
+    }
+
+}
